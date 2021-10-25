1b4fd6d5a9b462caf86727b185c29cc038064b57
frolix <kostya.lotysh@gmail.com>
2021-10-25 22:32:34 +0300

    add resource

--- app/src/main/java/com/example/reddittestapp/util/Resource.kt
+++ app/src/main/java/com/example/reddittestapp/util/Resource.kt
@@ -22,11 +22,3 @@ data class Resource<out T>(val status: Status, val data: T?, val message: String
         }
     }
 }
-//sealed class Resource<T>(
-//    val data: T? = null,
-//    val message: String? = null
-//) {
-//    class Success<T>(data: T) : Resource<T>(data)
-//    class Error<T>(message: String, data: T? = null) : Resource<T>(data, message)
-//    class Loading<T> : Resource<T>()
-//}
